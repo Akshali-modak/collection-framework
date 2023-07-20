fccf9269fa2b5a1df583aea3957aa602803e789a
bootcoding#123 <ModakAkshali211@gmail.com>
2023-07-20 15:46:45 +0530

    Remove Duplicate from arraylist using hashset

--- src/main/java/hashset/SortingDuplicateList.java
+++ src/main/java/hashset/SortingDuplicateList.java
@@ -10,9 +10,9 @@ public class SortingDuplicateList {
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
-        numbers.add(2);
-        numbers.add(4);
-        numbers.add(1);
+       // numbers.add(2);
+//        numbers.add(4);
+//        numbers.add(1);
 
         // Use a HashSet to remove duplicates
         HashSet<Integer> uniqueNumbers = new HashSet<>(numbers);
